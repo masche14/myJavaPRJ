923f85dc0784778e6036a80df2ea57640db20143
masche14 <zskfnxh134@gmail.com>
2024-03-25 14:15:59 +0900

    2024.03.25 실습4

--- src/main/java/Code04_13.java
+++ src/main/java/Code04_13.java
@@ -2,10 +2,15 @@ public class Code04_13 {
     public static void main(String[] args) {
         String str = " 한글 ABCD efgh ";
         String cutStr="";
+        String upper, lower;
 
+        upper = str.toUpperCase();
+        lower = str.toLowerCase();
         cutStr = str.trim();
 
-        System.out.println("기존 문자열 ["+str+"]");
-        System.out.println("공백 제거 ["+cutStr+"]");
+        System.out.println("기존 문자열 ==> ["+str+"]");
+        System.out.println("공백 제거 ==> ["+cutStr+"]");
+        System.out.println("대문자 ==> ["+upper+"]");
+        System.out.println("대문자 ==> ["+lower+"]");
     }
 }
