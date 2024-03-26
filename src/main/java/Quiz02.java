6e473b2225a16518b20eb9b709bcf729299f4fc4
masche14 <zskfnxh134@gmail.com>
2024-03-26 12:00:20 +0900

    퀴즈2차

--- src/main/java/Quiz02.java
+++ src/main/java/Quiz02.java
@@ -1,7 +1,7 @@
 public class Quiz02 {
     public static void main(String[] args) {
         for (int i=1; i<=7; i=i+2) {
-            for (int j=1; j<=i; j++){
+            for (int j=1; j<=7; j++){
                 if (j<=(7-i)/2) {
                     System.out.print(" ");
                 } else if (j>7-(7-i)/2){
@@ -10,9 +10,6 @@ public class Quiz02 {
                     System.out.print("*");
                 }
             } System.out.println();
-
         }
-
     }
-
 }
