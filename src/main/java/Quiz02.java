b012d5d31a05d33e1ad0c65d307a85da7b9740a6
masche14 <zskfnxh134@gmail.com>
2024-03-26 12:42:03 +0900

    퀴즈2차

--- src/main/java/Quiz02.java
+++ src/main/java/Quiz02.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Quiz02 {
+
     public static void main(String[] args) {
-        for (int i=1; i<=7; i=i+2) {
-            for (int j=1; j<=7; j++){
-                if (j<=(7-i)/2) {
+        Scanner s = new Scanner(System.in);
+        int num;
+        num = s.nextInt();
+        for (int i=1; i<=num; i=i+2) {
+            for (int j=1; j<=num; j++){
+                if (j<=(num-i)/2) {
                     System.out.print(" ");
-                } else if (j>7-(7-i)/2){
+                } else if (j>num-(num-i)/2){
                     System.out.print(" ");
                 } else {
                     System.out.print("*");
