f54b55e4cabc6aa3a8068cd2a5ddaf1365bef9ae
masche14 <zskfnxh134@gmail.com>
2024-04-01 13:36:58 +0900

    24.04.01 실습2

---
+++ src/main/java/Code05_02.java
@@ -0,0 +1,10 @@
+public class Code05_02 {
+    public static void main(String[] args){
+        int a = 99;
+
+        if (a<100) {
+            System.out.print("100보다 ");
+            System.out.print("작습니다.");
+        }
+    }
+}
