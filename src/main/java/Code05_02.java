8ce57e39e5b1ae24088cbfcf84ae7976f19b5742
masche14 <zskfnxh134@gmail.com>
2024-04-01 13:54:12 +0900

    24.04.01 실습2

--- src/main/java/Code05_02.java
+++ src/main/java/Code05_02.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Code05_02 {
     public static void main(String[] args){
-        int a = 99;
+        int a;
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
 
         if (a<100) {
             System.out.print("100보다 ");
