ecd759aa73201d415f9812d5b57a7ff92621f6d5
masche14 <zskfnxh134@gmail.com>
2024-04-01 15:05:41 +0900

    24.04.01 개인학습

---
+++ src/main/java/Study0401_2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Study0401_2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a;
+        while (true){
+            a = s.nextInt();
+            if (a==1) {
+                break;
+            }
+        }
+    }
+}
+
+
