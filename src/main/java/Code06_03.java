a7d5d7787274339029246ee45cbd8561b2102d62
masche14 <zskfnxh134@gmail.com>
2024-04-09 14:27:26 +0900

    2024.04.09 학교실습3

--- src/main/java/Code06_03.java
+++ src/main/java/Code06_03.java
@@ -4,7 +4,7 @@ public class Code06_03 {
             System.out.println(i+":난생 처음 자바는 재밋습니다.");
         }
         for (int i=3 ; i>0 ; i--){
-            System.out.println(i+":난생 처음 자바는 재밋습니다.");
+            System.out.println(i+":난생 처음 자바는 재밋습니다."); //
         }
     }
 }
