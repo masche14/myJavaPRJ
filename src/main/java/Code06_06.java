87a5aa0f12c49f0ef99f94164c58dc1aeb99e6de
masche14 <zskfnxh134@gmail.com>
2024-04-15 13:35:11 +0900

    2024.04.09 학교실습4

--- src/main/java/Code06_06.java
+++ src/main/java/Code06_06.java
@@ -7,7 +7,9 @@ public class Code06_06 {
         hap=0;
 
         while (true){
+            System.out.print("첫번째 정수를 입력하세요 ==> ");
             a = s.nextInt();
+            System.out.print("두번째 정수를 입력하세요 ==> ");
             b= s.nextInt();
 
             if (a<b) {
@@ -23,7 +25,7 @@ public class Code06_06 {
                     }
                 } break;
             } else {
-                System.out.println("값을 다시 입력하세요.");
+                System.out.println("두 정수의 값이 같습니다. 값을 다시 입력하세요.");
             }
         }
         System.out.printf("%d부터 %d까지의 합계 : %d \n", a, b, hap);
