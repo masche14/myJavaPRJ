c26bb7debf0e627d3aa875e8bb6bf6f95d196ce8
masche14 <zskfnxh134@gmail.com>
2024-04-15 13:45:15 +0900

    2024.04.15 학교 실습1

---
+++ src/main/java/Code06_09.java
@@ -0,0 +1,11 @@
+public class Code06_09 {
+    public static void main(String[] args) {
+        for (int i=0; i<3; i++){
+            for (int k=0; k<2; k++){
+                System.out.printf("난생 처음 쉽습니다. ^^ i : %d, k : %d \n", i, k);
+            }
+            System.out.println(i+"번째 반복문 종료");
+        }
+
+    }
+}
