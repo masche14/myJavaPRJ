3e9f81bbd8a0b5e4a71705cf1848ef4c1a157fb2
masche14 <zskfnxh134@gmail.com>
2024-04-15 15:12:46 +0900

    2024.04.15 학교 실습3

---
+++ src/main/java/Code06_11.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Code06_11 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int hap = 0;
+        int num1, num2;
+
+        while (true) {
+            System.out.print("첫번째 숫자 ==> ");
+            num1=s.nextInt();
+
+            if (num1==0){
+                break;
+            }
+
+            System.out.print("두번째 숫자 ==> ");
+            num2=s.nextInt();
+
+        }
+    }
+}
