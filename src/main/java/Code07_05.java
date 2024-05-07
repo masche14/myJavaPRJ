032bae94dd2f5a9a2d88a6107a55f03424815787
masche14 <zskfnxh134@gmail.com>
2024-05-07 13:29:12 +0900

    24.05.07 실습5

--- src/main/java/Code07_05.java
+++ src/main/java/Code07_05.java
@@ -1,5 +1,6 @@
 public class Code07_05 {
     public static void main(String[] args) {
+        // 거북이
         Turtle turtle1 = new Turtle();
         Turtle turtle2 = new Turtle();
         Turtle turtle3 = new Turtle();
