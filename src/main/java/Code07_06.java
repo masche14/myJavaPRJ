5c1d97df55a5c7d603e38bf9a38a82628ef19bfc
masche14 <zskfnxh134@gmail.com>
2024-05-07 14:17:57 +0900

    24.05.07 실습6

---
+++ src/main/java/Code07_06.java
@@ -0,0 +1,21 @@
+class Rabbit {
+    String shpae;
+    int xPos;
+    int yPos;
+
+    void setPosition(int x, int y){
+        this.xPos = x;
+        this.yPos = y;
+
+        System.out.printf("xPos : %d / yPos : %d \n", this.xPos, this.yPos);
+    }
+}
+public class Code07_06 {
+    public static void main(String[] args) {
+        Rabbit rb1 = new Rabbit();
+        rb1.setPosition(100, 100);
+
+        Rabbit rb2 = new Rabbit();
+        rb2.setPosition(200, 100);
+    }
+}
