4dd87b11ce7a3d3bdfb3453c91610620c0c55bdb
masche14 <zskfnxh134@gmail.com>
2024-05-07 15:07:26 +0900

    24.05.07 실습6

--- src/main/java/Code07_09.java
+++ src/main/java/Code07_09.java
@@ -3,8 +3,8 @@ class Rabbit1 {
     int xPos;
     int yPos;
 
-    Rabbit1(){
-        shape = "토끼";
+    Rabbit1(String value){
+        shape = value;
     }
 
     void setPosition(int xPos, int yPos){
@@ -16,12 +16,9 @@ class Rabbit1 {
 }
 public class Code07_09 {
     public static void main(String[] args) {
-        Rabbit1 rabbit1 = new Rabbit1();
-        Rabbit1 rabbit2 = new Rabbit1();
-        Rabbit1 rabbit3 = new Rabbit1();
-
-        rabbit1.shape = "원";
-        rabbit2.shape = "삼각형";
+        Rabbit1 rabbit1 = new Rabbit1("원");
+        Rabbit1 rabbit2 = new Rabbit1("삼각형");
+        Rabbit1 rabbit3 = new Rabbit1("토끼");
 
         rabbit1.setPosition(100,100);
         rabbit2.setPosition(-100,100);
