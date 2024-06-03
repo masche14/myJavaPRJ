833a37133d2ca35271c55acfdb3b40581ca3a4db
masche14 <zskfnxh134@gmail.com>
2024-06-03 13:31:12 +0900

    학교 실습 내용

--- src/main/java/Code08_10.java
+++ src/main/java/Code08_10.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 interface Car{
-    abstract void move();
+    void move();
+    void color(String color);
 }
 interface Cannon{
     abstract void fire();
@@ -9,6 +10,13 @@ interface Cannon{
 class Tank implements Car, Cannon{
     Scanner s = new Scanner(System.in);
     private int xPos, yPos;
+    private String color;
+
+    @Override
+    public void color(String color) {
+        this.color = color;
+        System.out.printf("%s 색상의 ", this.color);
+    }
     @Override
     public void move() {
         System.out.print("탱크를 이동시킬 좌표를 입력하세요 ==> ");
@@ -21,12 +29,15 @@ class Tank implements Car, Cannon{
     public void fire() {
         System.out.println("탱크가 발포를 시작합니다.");
     }
+
+
 }
 public class Code08_10 {
     public static void main(String[] args) {
 
         Tank tank = new Tank();
 
+        tank.color("노란색");
         tank.move();
         tank.fire();
 
